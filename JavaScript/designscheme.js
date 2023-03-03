34e913ff18537a7e06cb891738dcd98164d16f90
Maria Wakai <maria@Naoki-no-MacBook-Air-2.local>
2023-03-04 00:05:48 +0900

    design scheme

---
+++ JavaScript/designscheme.js
@@ -0,0 +1,9 @@
+let def = document.getElementById("default");
+let black = document.getElementById("black");
+let caramel = document.getElementById("caramel");
+let dark = document.getElementById("dark");
+
+function theme(bg, text){
+  document.body.style.background = bg;
+  document.body.style.color = text;
+}
